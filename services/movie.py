2613022b23d3e1bce6a6bd2ed5ee11639f4b393c
Maxe <mkasim.donchik@gmail.com>
2026-09-10 21:46:11 +0300

    Fix flake8 errors

--- services/movie.py
+++ services/movie.py
@@ -16,8 +16,10 @@ def get_movies(
 
     return queryset
 
+
 def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
+
 
 def create_movie(
         movie_title: str,
--- services/movie_session.py
+++ services/movie_session.py
@@ -3,7 +3,7 @@ from db.models import MovieSession
 
 
 def create_movie_session(
-        movie_show_time,
+        movie_show_time: str,
         movie_id: int,
         cinema_hall_id: int
 ) -> MovieSession:
@@ -13,18 +13,21 @@ def create_movie_session(
         cinema_hall_id=cinema_hall_id
     )
 
+
 def get_movies_sessions(session_date: str | None = None) -> QuerySet:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
+
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
+
 def update_movie_session(
         session_id: int,
-        show_time = None,
+        show_time: str | None = None,
         movie_id: int | None = None,
         cinema_hall_id: int | None = None
 ) -> MovieSession:
@@ -37,6 +40,7 @@ def update_movie_session(
         session.cinema_hall_id = cinema_hall_id
     session.save()
     return session
+
 
 def delete_movie_session_by_id(session_id: int) -> None:
     session = MovieSession.objects.get(id=session_id)
